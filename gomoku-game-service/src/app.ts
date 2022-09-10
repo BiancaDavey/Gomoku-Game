9097a366e6fd187b7710fbeb7b4e510f6d2df8b5
BiancaDavey <biancamdavey@icloud.com>
2022-09-10 13:55:47 +0200

    Install Mongoose and connect application to MongoDB

--- gomoku-game-service/src/app.ts
+++ gomoku-game-service/src/app.ts
@@ -1,14 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import gameHandler from './handler/game.handler';
+import connectDB from './util/connectDB';
 
 dotenv.config();
+//  Connect to database.
+connectDB();
 const app: Express = express();
 const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/games', gameHandler);
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+mongoose.connection.once('connected', () => {
+    console.log('⚡️[server]: Connected to MongoDB.');
+    app.listen(port, () => {
+      console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    });
+  })
