e9f27b198460b906b5bbb42959228f6c5a5e1b57
BiancaDavey <biancamdavey@icloud.com>
2022-10-26 14:43:58 +0200

    Add Theme context and buttons to switch theme.

--- gomoku-game-react/src/App.tsx
+++ gomoku-game-react/src/App.tsx
@@ -1,27 +1,29 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { Header, UserProvider } from './components'
+import { Header, UserProvider, ThemeProvider } from './components'
 import { Home, Game, Games, GameLog, Login, SignUp, Forum, CreatePost } from './pages'
 import style from './App.module.css'
 
 function App() {
   return (
     <UserProvider>
-      <Header />
-      <main className={style.main}>
-        <div className={style.container}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="game" element={<Game />} />
-            <Route path="games" element={<Games />} />
-            <Route path="game-log/:gameId" element={<GameLog />} />
-            <Route path="login" element={<Login/>} />
-            <Route path="sign-up" element={<SignUp/>} />
-            <Route path="forum" element={<Forum/>} />
-            <Route path="create-post" element={<CreatePost/>} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
-      </main>
+      <ThemeProvider>
+        <Header />
+        <main className={style.main}>
+          <div className={style.container}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="game" element={<Game />} />
+              <Route path="games" element={<Games />} />
+              <Route path="game-log/:gameId" element={<GameLog />} />
+              <Route path="login" element={<Login/>} />
+              <Route path="sign-up" element={<SignUp/>} />
+              <Route path="forum" element={<Forum/>} />
+              <Route path="create-post" element={<CreatePost/>} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
+        </main>
+      </ThemeProvider>
     </UserProvider>
   )
 }
--- gomoku-game-react/src/types/index.ts
+++ gomoku-game-react/src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './GameDetails'
 export * from './User'
+export * from './Theme'
